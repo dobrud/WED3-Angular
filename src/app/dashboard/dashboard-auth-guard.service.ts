c2ae6cc6550f006082dab9d5dcef631197bcb329
Dominik Bruderer <dominikbruderer@gmail.com>
2017-04-12 16:43:09 +0200

    Redirect user directly to welcome if he wants to access guarded part of application with no authentication

--- src/app/dashboard/dashboard-auth-guard.service.ts
+++ src/app/dashboard/dashboard-auth-guard.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 import { CanLoad } from '@angular/router';
-import { AuthService } from '../auth/services/auth.service'
+import { AuthService } from '../auth/services';
+import { NavigationService } from '../core';
 
 @Injectable()
 export class DashboardAuthGuard implements CanLoad {
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private navigationService: NavigationService) {}
 
   canLoad() {
-    return this.authService.hasCredentials;
+    if(this.authService.hasCredentials) {
+      return true;
+    } else {
+      this.navigationService.goToUrl('/welcome');
+      return false;
+    }
   }
 }
