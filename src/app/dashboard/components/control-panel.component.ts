489f72d0746ff29bd7fa8b370c1665cabdbd3c2d
Dominik Bruderer <dominikbruderer@gmail.com>
2017-04-14 21:39:50 +0200

    Load bank account information

--- src/app/dashboard/components/control-panel.component.ts
+++ src/app/dashboard/components/control-panel.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { BankAccount, TransactionInfo, Transaction } from '../../shared/models';
+import { AuthService } from '../../auth/services';
+import { BankAccountService } from '../../shared/services';
 
 @Component({
   selector: 'app-control-panel',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./control-panel.component.css']
 })
 export class ControlPanelComponent implements OnInit {
+  private ownAccount: BankAccount;
 
-  constructor() { }
+  constructor(private bankAccountService: BankAccountService) {
+    this.ownAccount = new BankAccount(null, null, null, null);
+  }
 
   ngOnInit() {
+    this.getOwnAccount();
   }
+
+  getOwnAccount() {
+    this.bankAccountService.getOwnBankAccount().subscribe(
+      (data:BankAccount) => {
+        this.ownAccount = data;
+    } );
+  }
+
 
 }
